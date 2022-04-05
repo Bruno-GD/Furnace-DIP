ade6593b8e419e0c538f8c5ab188fa9d2ec254a9
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-04-05 17:47:06 +0200

    :recycle: actualizado el Regulator.java para usar un "ThermalDisplay"

--- src/main/java/edu/poniperro/furnace/thermalregulators/Regulator.java
+++ src/main/java/edu/poniperro/furnace/thermalregulators/Regulator.java
@@ -1,29 +1,34 @@
 package edu.poniperro.furnace.thermalregulators;
 
 import edu.poniperro.furnace.heaters.IHeater;
+import edu.poniperro.furnace.thermaldisplays.BasicDisplay;
+import edu.poniperro.furnace.thermaldisplays.IThermalDisplay;
 import edu.poniperro.furnace.thermalplaces.IThermalPlace;
 import edu.poniperro.furnace.thermalsensors.IThermalSensor;
 
 public class Regulator implements IThermalRegulator {
+    private final IThermalDisplay screen = new BasicDisplay();
 
     @Override
     public void regulate(IThermalSensor sensor, IHeater heater, IThermalPlace place, double maxTemperature, double minTemperature) {
         double current = sensor.getCurrentTemperatureOf(place);
+        screen.display(RegulatorState.TURN_ON, current);
         // increase
-        while (current <= maxTemperature) {
+        while (current < maxTemperature) {
             // update temperature
             heater.increaseTemperatureOf(place);
             current = sensor.getCurrentTemperatureOf(place);
-            // TODO better way to show temperature
-            System.out.printf("Temperatura actual: %.2fºC%n", current);
+            // display temperature
+            screen.display(RegulatorState.HEATING, current);
         }
+        screen.display(RegulatorState.OFF, current);
         // decrease
-        while (current >= minTemperature) {
+        while (current > minTemperature) {
             // update temperature
             place.freeze();
             current = sensor.getCurrentTemperatureOf(place);
-            // TODO better way to show temperature
-            System.out.printf("Temperatura actual: %.2fºC%n", current);
+            // display temperature
+            screen.display(RegulatorState.WAITING, current);
         }
     }
 }
