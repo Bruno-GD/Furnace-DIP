4bc701d66c7615fd753a1500b5a34926f57e8cc0
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-04-05 17:19:19 +0200

    :white_check_mark: Primer test en App.java

--- src/main/java/edu/poniperro/furnace/App.java
+++ src/main/java/edu/poniperro/furnace/App.java
@@ -1,8 +1,27 @@
 package edu.poniperro.furnace;
 
+import edu.poniperro.furnace.heaters.GasHeater;
+import edu.poniperro.furnace.heaters.IHeater;
+import edu.poniperro.furnace.thermalplaces.IThermalPlace;
+import edu.poniperro.furnace.thermalplaces.RoomTemperature;
+import edu.poniperro.furnace.thermalregulators.IThermalRegulator;
+import edu.poniperro.furnace.thermalregulators.Regulator;
+import edu.poniperro.furnace.thermalsensors.IThermalSensor;
+import edu.poniperro.furnace.thermalsensors.RemoteCommandSensor;
+
 public class App {
 
     public static void main(String[] args) {
+        final double minTemp = 15d;
+        final double maxTemp = 21d;
 
+        IThermalPlace room = new RoomTemperature(15d);
+        IHeater gasHeater = new GasHeater();
+        IThermalSensor thermometer = new RemoteCommandSensor();
+
+        IThermalRegulator regulator = new Regulator();
+
+        System.out.println("Arrancando...");
+        regulator.regulate(thermometer, gasHeater, room, maxTemp, minTemp);
     }
 }
