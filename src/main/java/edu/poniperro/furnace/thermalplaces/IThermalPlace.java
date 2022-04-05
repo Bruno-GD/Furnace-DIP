18d8e470fdabbfee90cf79063d42e186632fc637
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-04-05 17:04:51 +0200

    :building_construction: nuevo método "freeze" en los "ThermalPlace"

--- src/main/java/edu/poniperro/furnace/thermalplaces/IThermalPlace.java
+++ src/main/java/edu/poniperro/furnace/thermalplaces/IThermalPlace.java
@@ -1,6 +1,19 @@
 package edu.poniperro.furnace.thermalplaces;
 
 public interface IThermalPlace {
+
+    /**
+     * @return the temperature of the place (requires a IThermalSensor)
+     */
     double getTemperature();
+
+    /**
+     * @param celsius the celsius to increase
+     */
     void updateTemperature(double celsius);
+
+    /**
+     * When there's no heater, the place will decrease the temperature!
+     */
+    void freeze();
 }
