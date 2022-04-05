5012d469251783d9d87b98daef234f2671021e36
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-04-05 16:44:38 +0200

    :sparkles: nuevo "Heater" GasHeater.java

---
+++ src/main/java/edu/poniperro/furnace/heaters/GasHeater.java
@@ -0,0 +1,12 @@
+package edu.poniperro.furnace.heaters;
+
+import edu.poniperro.furnace.thermalplaces.IThermalPlace;
+
+public class GasHeater implements IHeater {
+    private final double increasePerTick = 1d;
+
+    @Override
+    public void increaseTemperatureOf(IThermalPlace place) {
+        place.updateTemperature(place.getTemperature() + increasePerTick);
+    }
+}
